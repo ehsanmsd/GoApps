e6785f965cb0b2082bc68262f0df84a15f66792b
ehsanmsd <azimmasoudi@gmail.com>
2022-11-26 17:18:02 +0100

    Using slices

--- go-cards/main.go
+++ go-cards/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 
 }
 
